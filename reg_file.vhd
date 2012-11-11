ce20b3b3ec15578c350c9fc8bbd6f8394ff33add
jctemkin <jcs@jennspence.com>
2012-11-11 01:45:53 -0500

    Startup with PCM halted

--- reg_file.vhd
+++ reg_file.vhd
@@ -25,7 +25,7 @@ end reg_file;
 architecture Behavioral of reg_file is
 
 	type register_type is array(0 to ((2 ** addr_len) - 1)) of std_logic_vector((data_len - 1) downto 0);
-	signal regs: register_type := (others => (others => '0'));
+	signal regs: register_type := (0=>X"FF", 1=>X"FF", 2=>X"FF", 3=>X"FF", others => (others => '0'));
 	
 	signal rd_data_reg: std_logic_vector((data_len - 1) downto 0) := (others => '0');
 
