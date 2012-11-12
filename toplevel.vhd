94dbc900c678960ca568ef5d1653c80f5857723a
jctemkin <jcs@jennspence.com>
2012-11-12 00:57:58 -0500

    Test commit

--- toplevel.vhd
+++ toplevel.vhd
@@ -2,10 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity toplevel is
 	Port ( 
