a09bd02f096558a785c08a50b00b027a5b856267
jctemkin <jcs@jennspence.com>
2012-12-04 17:44:58 -0500

    Attitude calc algorithm partially mapped to hardware

--- attitude_calc.vhd
+++ attitude_calc.vhd
@@ -2,6 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+Library UNISIM;
+use UNISIM.vcomponents.all;
+
 
 entity attitude_calc is
 	port (
@@ -15,73 +18,30 @@ end attitude_calc;
 
 architecture Behavioral of attitude_calc is
 
-	signal ax_raw: std_logic_vector(15 downto 0);
-	signal ay_raw: std_logic_vector(15 downto 0);
-	signal az_raw: std_logic_vector(15 downto 0);
+	signal ax_offset, ay_offset, az_offset: std_logic_vector(17 downto 0);
+	signal gx_offset, gy_offset, gz_offset: std_logic_vector(17 downto 0);
+	signal Dt: std_logic_vector(17 downto 0);
+	signal Kd: std_logic_vector(17 downto 0);
+	signal Kp: std_logic_vector(17 downto 0);
 	
-	signal ax_offset: std_logic_vector(15 downto 0);
-	signal ay_offset: std_logic_vector(15 downto 0);
-	signal az_offset: std_logic_vector(15 downto 0);
-	
-	signal axc: std_logic_vector(15 downto 0);
-	signal ayc: std_logic_vector(15 downto 0);
-	signal azc: std_logic_vector(15 downto 0);
-	
-	signal acc_pitch: std_logic_vector(15 downto 0);
-	signal acc_roll: std_logic_vector(15 downto 0);
-	
-	
-	
-	signal gx_raw: std_logic_vector(15 downto 0);
-	signal gy_raw: std_logic_vector(15 downto 0);
-	signal gz_raw: std_logic_vector(15 downto 0);
-	
-	signal gx_offset: std_logic_vector(15 downto 0);
-	signal gy_offset: std_logic_vector(15 downto 0);
-	signal gz_offset: std_logic_vector(15 downto 0);
-	
-	signal gxc: std_logic_vector(15 downto 0);
-	signal gyc: std_logic_vector(15 downto 0);
-	signal gzc: std_logic_vector(15 downto 0);
-	
-	signal gx_scale: std_logic_vector(15 downto 0);
-	signal gy_scale: std_logic_vector(15 downto 0);
-	signal gz_scale: std_logic_vector(15 downto 0);
-	
-	signal gx: std_logic_vector(15 downto 0);
-	signal gy: std_logic_vector(15 downto 0);
-	signal gz: std_logic_vector(15 downto 0);
+
+	signal ax_raw, ayraw, azraw: std_logic_vector(17 downto 0);
+	signal axc, ayc, azc: std_logic_vector(17 downto 0);
+	signal acc_pitch, acc_roll: std_logic_vector(17 downto 0);
+		
+	signal gx_raw, gy_raw, gz_raw: std_logic_vector(17 downto 0);
+	signal gx_scale, gy_scale, gz_scale: std_logic_vector(17 downto 0);	--scale gyro readings to be radians / ms, with 3 integer places (counting sign bit)
+	signal gx, gy, gz: std_logic_vector(17 downto 0);
+	signal gx_int, gy_int: std_logic_vector(17 downto 0);
 	
 	
-	signal Dt: std_logic_vector(15 downto 0);
-	
-	signal gxt: std_logic_vector(15 downto 0);
-	signal gyt: std_logic_vector(15 downto 0);
-	
-	
-	signal last_pitch: std_logic_vector(15 downto 0);
-	signal last_roll: std_logic_vector(15 downto 0);
-	
-	signal gx_int: std_logic_vector(15 downto 0);
-	signal gy_int: std_logic_vector(15 downto 0);
-	
-	
-	signal Kd: std_logic_vector(15 downto 0);
-	signal Kp: std_logic_vector(15 downto 0);
-	
-	signal p_pitch: std_logic_vector(15 downto 0);
-	signal d_pitch: std_logic_vector(15 downto 0);
-	
-	signal p_roll: std_logic_vector(15 downto 0);
-	signal d_roll: std_logic_vector(15 downto 0);
-	
-	signal pitch: std_logic_vector(15 downto 0);
-	signal roll: std_logic_vector(15 downto 0);
-	
-	signal atan_x: std_logic_vector(15 downto 0);	--X and Y must have same binary point location.
-	signal atan_y: std_logic_vector(15 downto 0);
-	signal atan_out: std_logic_vector(15 downto 0);	--Output in fixed-point 2's complement radians, with 3 integer places.
-	signal atan_rdy: std_logic;
+	signal last_pitch, last_roll: std_logic_vector(17 downto 0);
+	signal p_pitch, d_pitch: std_logic_vector(17 downto 0);	
+	signal p_roll, d_roll: std_logic_vector(17 downto 0);
+	signal pitch, roll: std_logic_vector(17 downto 0);
+	signal m_pitch, m_roll: std_logic_vector(17 downto 0);
+
+	signal atan_rdy1, atan_rdy2: std_logic;
 	
 	
 	component arctan
@@ -95,75 +55,181 @@ architecture Behavioral of attitude_calc is
 	end component;
 
 begin
-	
-	
-	--
-	--Add stage: add constants to raw sensor values	
-	gxc <= gx_raw + gx_offset;
-	gyc <= gy_raw + gy_offset;
-	
-	--Multiply stage: scale gyro readings
-	gx <= gxc * gx_scale;
-	gy <= gyc * gy_scale;
+	--=======================--
+	-- Load data from register file
+	ax_raw <= std_logic_vector(resize(signed(read_regs(15 downto 0)), 18));
+	ay_raw <= std_logic_vector(resize(signed(read_regs(31 downto 16)), 18));
+	az_raw <= std_logic_vector(resize(signed(read_regs(47 downto 32)), 18));
 	
 	
 	
-	--Multiply stage: multiply gyro readings by time
-	gxt <= gx_scale * Dt;
-	gyt <= gy_scale * Dt;
 	
-	--Add stage: add gyro*time readings to last recorded attitude
-	gx_int <= gxt + last_pitch;
-	gy_int <= gxy + last_roll;
 	
-	--Multiply stage: Multiply by complimentary filter constants
-	p_pitch <= gx_int * Kp;
-	p_roll <= gy_int * Kp;
+	
+	
+	
+	--=======================--
+	-- Gyro processing section
+	-- Pitch:
+	
+	--Preadd-multiply: op=0x11; P=a*(b+d) = (gx_raw + gx_offset) * gx_scale
+	--TODO: correct for overflow
+	dsp_gx1: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gx_scale,
+			b => gx_raw,
+			d => gx_offset,
+			p => gx,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"11"	
+		);
+	
+	--Multiply-postadd: op=0x1D; P=c+(a*b) = (gx * Dt) + last_pitch
+	dsp_gx2: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gx,
+			b => Dt,
+			c => last_pitch,
+			p => gx_int,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"1D"	
+		);
+		
+	--Multiply: op=0x01; C_P_out=a*b = gx_int * Kp (cascade to dsp_pitch)
+	dsp_p_pitch: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gx_int,
+			b => Kp,
+			c_p_out => p_pitch,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"01"	
+		);
+		
+		
+	------------------------
+	--Roll:
+
+	--Preadd-multiply: op=0x11; P=a*(b+d) = (gy_raw + gy_offset) * gy_scale
+	dsp_gy1: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gy_scale,
+			b => gy_raw,
+			d => gy_offset,
+			p => gy,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"11"
+		);
+	
+	--Multiply-postadd: op=0x1D; P=c+(a*b) = (gy * Dt) + last_roll
+	dsp_gy2: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gy,
+			b => Dt,
+			c => last_roll,
+			p => gy_int,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"1D"	
+		);
+	
+	--Multiply: op=0x01; C_P_out=a*b = gy_int * Kp (cascade to dsp_roll)
+	dsp_p_roll: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => gy_int,
+			b => Kp,
+			c_p_out => p_roll,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"01"	
+		);
+	
 	
 	
 	--=========================--
+	-- Accelerometer section
 	
-	--Add stage: add constants to raw sensor values	
-	axc <= ax_raw + ax_offset;
-	ayc <= ay_raw + ay_offset;
-	azc <= az_raw + az_offset;
+	--Add stage: add constants to raw sensor values
+	--TODO: prevent overflow
+	axc <= signed(ax_raw) + signed(ax_offset);
+	ayc <= signed(ay_raw) + signed(ay_offset);
+	azc <= signed(az_raw) + signed(az_offset);
 	
 	--Trig stage: take arctangent of offset sensor values
-	atan_x1 <= axc;
-	atan_y1 <= azc;
-	atan_x2 <= ayc;
-	atan_y2 <= azc;
-	
-	--Multiply stage: Multiply by complimentary filter constants
-	d_pitch <= atan_out1 * Kd;
-	d_roll <= atan_out2 * Kd;
-	
-	
-	--=========================--
-	
-	--Add stage: Add P and D terms to obtain final pitch and roll
-	pitch <= p_pitch + d_pitch;
-	roll <= p_roll + d_roll;
-	
-	
-	
 	arctan_i1: arctan
 		port map(
-			x_in => atan_x1,
-			y_in => atan_y1,
-			phase_out => atan_out1,
+			x_in => axc,
+			y_in => azc,
+			phase_out => acc_pitch,
 			rdy => atan_rdy1,
 			clk => clk
 		);
 		
 	arctan_i2: arctan
 		port map(
-			x_in => atan_x2,
-			y_in => atan_y2,
-			phase_out => atan_out2,
+			x_in => ayc,
+			y_in => azc,
+			phase_out => acc_roll,
 			rdy => atan_rdy2,
 			clk => clk
 		);
+	
+	
+	
+	
+	--=========================--
+	-- Combine gyro and accelerometer data
+	
+	--Cascade multiply-postadd: op=0x05; P=P_in+(a*b) = (atan_out1 * Kd) + p_pitch
+	dsp_pitch: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => acc_pitch,
+			b => Kd,
+			c_p_in => p_pitch,
+			p => pitch,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"05"	
+		);
+		
+	--Cascade multiply-postadd: op=0x05; P=P_in+(a*b) = (atan_out2 * Kd) + p_roll
+	dsp_roll: entity work.dsp48a1_wrapper(Behavioral)
+		port map(
+			a => acc_roll,
+			b => Kd,
+			c_p_in => p_roll,
+			p => roll,
+			
+			rst => rst,
+			clk => clk,
+			clk_en => clk_en,
+			opmode => X"05"	
+		);
+	
+	--Add stage: correct for mounting angle
+	--TODO: overflow
+	m_pitch <= signed(pitch) + signed(pitch_corr);
+	m_roll <= signed(roll) + signed(roll_corr);
+	
+	
+
 
 
 
---
+++ dsp48a1_wrapper.vhd
@@ -0,0 +1,116 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:12:03 12/04/2012 
+-- Design Name: 
+-- Module Name:    dsp48a1_wrapper - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity dsp48a1_wrapper is
+	port (
+		--Fabric ports
+		a: in std_logic_vector(17 downto 0);
+		b: in std_logic_vector(17 downto 0);
+		c: in std_logic_vector(47 downto 0);
+		d: in std_logic_vector(17 downto 0);
+		p: out std_logic_vector(47 downto 0);
+		c_out: out std_logic;
+		m: out std_logic_vector(35 downto 0);	--Output of pre-add and multiply
+		
+		--Cascade ports
+		c_p_in: in std_logic_vector(47 downto 0);		--Input to post-adder
+		c_p_out: out std_logic_vector(47 downto 0);	--Output of post-adder
+		
+		--Control ports
+		rst: in std_logic;
+		clk: in std_logic;
+		clk_en: in std_logic;
+		opmode: in std_logic(7 downto 0)
+	);
+end dsp48a1_wrapper;
+
+architecture Behavioral of dsp48a1_wrapper is
+
+begin
+
+
+	DSP48A1_inst : DSP48A1
+		generic map (
+			A0REG => 0,              -- First stage A input pipeline register (0/1)
+			A1REG => 1,              -- Second stage A input pipeline register (0/1)
+			B0REG => 0,              -- First stage B input pipeline register (0/1)
+			B1REG => 1,              -- Second stage B input pipeline register (0/1)
+			CARRYINREG => 0,         -- CARRYIN input pipeline register (0/1)
+			CARRYINSEL => "OPMODE5", -- Specify carry-in source, "CARRYIN" or "OPMODE5" 
+			CARRYOUTREG => 1,        -- CARRYOUT output pipeline register (0/1)
+			CREG => 1,               -- C input pipeline register (0/1)
+			DREG => 1,               -- D pre-adder input pipeline register (0/1)
+			MREG => 1,               -- M pipeline register (0/1)
+			OPMODEREG => 1,          -- Enable=1/disable=0 OPMODE input pipeline registers
+			PREG => 1,					 -- P output pipeline register (0/1)
+			RSTTYPE => "SYNC"        -- Specify reset type, "SYNC" or "ASYNC" 
+		)
+		port map (
+			-- Cascade Ports: 18-bit (each) output: Ports to cascade from one DSP48 to another
+			--BCOUT => BCOUT,           -- 18-bit output: B port cascade output
+			PCOUT => c_p_out,           -- 48-bit output: P cascade output (if used, connect to PCIN of another DSP48A1)
+			
+			-- Data Ports: 1-bit (each) output: Data input and output ports
+			--CARRYOUT => c_carry_out,     -- 1-bit output: carry output (if used, connect to CARRYIN pin of another DSP48A1)
+			CARRYOUTF => carry_out,   -- 1-bit output: fabric carry output
+			M => m,                   -- 36-bit output: fabric multiplier data output
+			P => p,                   -- 48-bit output: data output
+			
+			-- Cascade Ports: 48-bit (each) input: Ports to cascade from one DSP48 to another
+			PCIN => c_p_in,             -- 48-bit input: P cascade input (if used, connect to PCOUT of another DSP48A1)
+			
+			-- Control Input Ports: 1-bit (each) input: Clocking and operation mode
+			CLK => clk,               -- 1-bit input: clock input
+			OPMODE => opmode,         -- 8-bit input: operation mode input
+			
+			-- Data Ports: 18-bit (each) input: Data input and output ports
+			A => a,                   -- 18-bit input: A data input
+			B => b,                   -- 18-bit input: B data input (connected to fabric or BCOUT of adjacent DSP48A1)
+			C => c,                   -- 48-bit input: C data input
+			--CARRYIN => c_carry_in,       -- 1-bit input: carry input signal (if used, connect to CARRYOUT pin of another DSP48A1)
+			D => d,                   -- 18-bit input: B pre-adder data input
+			
+			-- Reset/Clock Enable Input Ports: 1-bit (each) input: Reset and enable input ports
+			CEA => clk_en,               -- 1-bit input: active high clock enable input for A registers
+			CEB => clk_en,               -- 1-bit input: active high clock enable input for B registers
+			CEC => clk_en,               -- 1-bit input: active high clock enable input for C registers
+			CECARRYIN => clk_en,		-- 1-bit input: active high clock enable input for CARRYIN registers
+			CED => clk_en,               -- 1-bit input: active high clock enable input for D registers
+			CEM => clk_en,               -- 1-bit input: active high clock enable input for multiplier registers
+			CEOPMODE => clk_en,     -- 1-bit input: active high clock enable input for OPMODE registers
+			CEP => clk_en,               -- 1-bit input: active high clock enable input for P registers
+			RSTA => rst,             -- 1-bit input: reset input for A pipeline registers
+			RSTB => rst,             -- 1-bit input: reset input for B pipeline registers
+			RSTC => rst,             -- 1-bit input: reset input for C pipeline registers
+			RSTCARRYIN => rst, -- 1-bit input: reset input for CARRYIN pipeline registers
+			RSTD => rst,             -- 1-bit input: reset input for D pipeline registers
+			RSTM => rst,             -- 1-bit input: reset input for M pipeline registers
+			RSTOPMODE => rst,   -- 1-bit input: reset input for OPMODE pipeline registers
+			RSTP => rst              -- 1-bit input: reset input for P pipeline registers
+		);
+
+end Behavioral;
+
